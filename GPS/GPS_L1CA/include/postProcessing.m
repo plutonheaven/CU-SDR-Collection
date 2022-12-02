5c00ee0d448bea4e695fd1301679a63277f1e457
Paul Thevenon <thevenon@recherche.enac.fr>
2022-12-02 16:18:09 +0100

    - restored initial files for init.m, initSettings.m, acquisition.m, postProcessing.m

--- GPS/GPS_L1CA/include/postProcessing.m
+++ GPS/GPS_L1CA/include/postProcessing.m
@@ -14,7 +14,6 @@
 % Based on the original work by Darius Plausinaitis,Peter Rinder, 
 % Nicolaj Bertelsen and Dennis M. Akos
 %--------------------------------------------------------------------------
-
 %This program is free software; you can redistribute it and/or
 %modify it under the terms of the GNU General Public License
 %as published by the Free Software Foundation; either version 2
@@ -43,9 +42,9 @@
 % 3.1) Initialize channels (preRun.m).
 % 3.2) Pass the channel structure and the file identifier to the tracking
 % function. It will read and process the data. The tracking results are
-% stored in the trackResults structure. The results can be accessed this
+% stored in the trkResults structure. The results can be accessed this
 % way (the results are stored each millisecond):
-% trackResults(channelNumber).XXX(fromMillisecond : toMillisecond), where
+% trkResults(channelNumber).XXX(fromMillisecond : toMillisecond), where
 % XXX is a field name of the result (e.g. I_P, codePhase etc.)
 %
 % 4) Pass tracking results to the navigation solution function. It will
@@ -61,98 +60,100 @@ disp ('Starting processing...');
 
 %Initialize the multiplier to adjust for the data type
 if (settings.fileType==1) 
-    dataAdaptCoeff=1;
+dataAdaptCoeff=1;
 else
-    dataAdaptCoeff=2;
+dataAdaptCoeff=2;
 end
 
 %If success, then process the data
 if (fid > 0)
-    
-    % Move the starting point of processing. Can be used to start the
-    % signal processing at any point in the data record (e.g. good for long
-    % records or for signal processing in blocks).
-    fseek(fid, dataAdaptCoeff*settings.skipNumberOfBytes, 'bof'); 
+
+% Move the starting point of processing. Can be used to start the
+% signal processing at any point in the data record (e.g. good for long
+% records or for signal processing in blocks).
+fseek(fid, dataAdaptCoeff*settings.skipNumberOfBytes, 'bof'); 
 
 %% Acquisition ============================================================
 
-    % Do acquisition if it is not disabled in settings or if the variable
-    % acqResults does not exist.
-    if ((settings.skipAcquisition == 0) || ~exist('acqResults', 'var'))
-        
-        % Find number of samples per spreading code
-        samplesPerCode = round(settings.samplingFreq / ...
-                           (settings.codeFreqBasis / settings.codeLength));
-        
-        
-        % At least 42ms of signal are needed for fine frequency estimation
-        codeLen = max(42,2*(settings.acqNonCohTime*settings.acqCohTime)+2);
-        % Read data for acquisition.
-        data  = fread(fid, dataAdaptCoeff*codeLen*samplesPerCode, settings.dataType)';
+% Do acquisition if it is not disabled in settings or if the variable
+% acqResults does not exist.
+if ((settings.skipAcquisition == 0) || ~exist('acqResults', 'var'))
     
-        if (dataAdaptCoeff==2)    
-            data1=data(1:2:end);    
-            data2=data(2:2:end);    
-            data=data1 + 1i .* data2;
-        end
+    % Find number of samples per spreading code
+    samplesPerCode = round(settings.samplingFreq / ...
+                       (settings.codeFreqBasis / settings.codeLength));
+    
+    
+    % At least 42ms of signal are needed for fine frequency estimation
+    codeLen = max(42,settings.acqNonCohTime+2);
+    % Read data for acquisition.
+    data  = fread(fid, dataAdaptCoeff*codeLen*samplesPerCode, settings.dataType)';
 
-        %--- Do the acquisition -------------------------------------------
-        disp ('   Acquiring satellites...');
-        acqResults = acquisition(data, settings);
+    if (dataAdaptCoeff==2)    
+        data1=data(1:2:end);    
+        data2=data(2:2:end);    
+        data=data1 + 1i .* data2;    
     end
+
+    %--- Do the acquisition -------------------------------------------
+    disp ('   Acquiring satellites...');
+    acqResults = acquisition(data, settings);
+    save("acqResults")
+end
 
 %% Initialize channels and prepare for the run ============================
 
-    % Start further processing only if a GNSS signal was acquired (the
-    % field FREQUENCY will be set to 0 for all not acquired signals)
-    if (any(acqResults.carrFreq))
-        channel = preRun(acqResults, settings);
-        showChannelStatus(channel, settings);
-    else
-        % No satellites to track, exit
-        disp('No GNSS signals detected, signal processing finished.');
-        trackResults = [];
-        navSolutions = [];
-%         return;
-    end
+% Start further processing only if a GNSS signal was acquired (the
+% field FREQUENCY will be set to 0 for all not acquired signals)
+if (any(acqResults.carrFreq))
+    channel = preRun(acqResults, settings);
+    showChannelStatus(channel, settings);
+else
+    % No satellites to track, exit
+    disp('No GNSS signals detected, signal processing finished.');
+    trkResults = [];
+    navResults = [];
+    return;
+end
 
-%%% Track the signal =======================================================
-% startTime = now;
-% disp (['   Tracking started at ', datestr(startTime)]);
-% 
-% % Process all channels for given data block
-% [trkResults, ~] = tracking(fid, channel, settings);
-% save("trkResults")
+%% Track the signal =======================================================
+startTime = now;
+disp (['   Tracking started at ', datestr(startTime)]);
+
+% Process all channels for given data block
+[trkResults, ~] = tracking(fid, channel, settings);
+save("trkResults")
 % Close the data file
 fclose(fid);
-% disp(['   Tracking is over (elapsed time ', ...
-%                                     datestr(now - startTime, 13), ')'])                      
-% 
-% %% Calculate navigation solutions =========================================
-% disp('   Calculating navigation solutions...');
-% 
-% [navResults, ~] = postNavigation(trkResults, settings);
-% save("navResults")
-% 
-% disp('   Processing is complete for this data block');
-% disp('Post processing of the signal is over.');
+disp(['   Tracking is over (elapsed time ', ...
+                                    datestr(now - startTime, 13), ')'])                      
+
+%% Calculate navigation solutions =========================================
+disp('   Calculating navigation solutions...');
+
+[navResults, ~] = postNavigation(trkResults, settings);
+save("navResults")
+
+disp('   Processing is complete for this data block');
+disp('Post processing of the signal is over.');
 %% Plot all results ===================================================
 disp ('   Ploting results...');
 
 if settings.plotAcquisition
     plotAcquisition(acqResults);
-    plotAcquisitionMat(acqResults,settings,settings.acqSatelliteList);
 end
 
-% if settings.plotTracking
-%     plotTracking(1:settings.numberOfChannels, trkResults, settings);
-% end
-% 
-% if settings.plotNavigation
-%     plotNavigation(navResults, settings);
-% end
+if settings.plotTracking
+    plotTracking(1:settings.numberOfChannels, trkResults, settings);
+end
+
+if settings.plotNavigation
+    plotNavigation(navResults, settings);
+end
 disp('Post processing of the signal is over.');
+
 else
-    % Error while opening the data file.
-    error('Unable to read file %s: %s.', settings.fileName, message);
+% Error while opening the data file.
+error('Unable to read file %s: %s.', settings.fileName, message);
 end % if (fid > 0)
+
